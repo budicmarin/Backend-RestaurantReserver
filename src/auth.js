dcdf80f59dbc4dae4721c079151c266211009980
Administrator <markovici089@gmail.com>
2024-01-25 11:46:52 +0100

    heshirani password

--- src/auth.js
+++ src/auth.js
@@ -1,17 +1,19 @@
 import mongo from "mongodb";
 import db from "./db.js";
 import { ObjectId } from "mongodb";
+import bcrypt from "bcrypt";
 
 const gostiCollection = db.collection("Gosti");
 
 export default {
   async registerGost(gostData) {
+    const hashedPassword = await bcrypt.hash(gostData.password, 10);
     await gostiCollection.insertOne({
       _id: new ObjectId(),
       firstName: gostData.firstName,
       lastName: gostData.lastName,
       email: gostData.email,
-      password: gostData.password,
+      password: hashedPassword,
     });
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import db from "./db.js";
 import auth from "./auth.js";
+import { radniciMethods } from "../Handlers/radniciHandler.js";
+import { gostiMethods } from "../Handlers/gostiHandler.js";
+import { ocjeneMethods } from "../Handlers/ocjeneHandler.js";
+import { piceMethods } from "../Handlers/piceHandler.js";
 
 const app = express(); // instanciranje aplikacije
 const port = 3000; // port na kojem će web server slušati
@@ -23,12 +27,31 @@ app.post("/registracija", (req, res) => {
   res.status(201).send({ message: "OK" });
 });
 
-app.get("/gost", (req, res) => {
-  res.json([{}]);
+app.get("/gost/:id", (req, res) => {
+  res.json([{ id: 5 }]);
 });
 
-app.get("/gost/:gostId", (req, res) => {
-    res.json([{ gostId:5 }]);
-  });
+// Pozivanje nekih osnovnih operacija radi provjere konekcije
+// sa MongomDB
+
+app.get("/radnici", radniciMethods.getAllRadnici);
+app.get("/radnici/:id", radniciMethods.getRadnikById);
+app.post("/radnici", radniciMethods.newRadnik);
+app.delete("/radnici/:id", radniciMethods.deleteRadnik);
+
+app.get("/gosti", gostiMethods.getAllGosti);
+app.get("/gosti/:id", gostiMethods.getGostById);
+//app.post("/gosti", gostiMethods.newGost);
+app.delete("/gosti/:id", gostiMethods.deleteGost);
+
+app.get("/ocjene", ocjeneMethods.getAllOcjene);
+app.get("/ocjene/:id", ocjeneMethods.getOcjenaById);
+app.post("/ocjene", ocjeneMethods.newOcjena);
+app.delete("/ocjene/:id", ocjeneMethods.deleteOcjena);
+
+app.get("/pice", piceMethods.getAllPica);
+app.get("/pice/:id", piceMethods.getPiceById);
+app.post("/pice", piceMethods.newPice);
+app.delete("/pice/:id", piceMethods.deletePice);
 
 app.listen(port, () => console.log(`Slušam na portu ${port}!`));
